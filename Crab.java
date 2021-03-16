ace7290d9c51c668953775c31ce8d16ba8d7366b
maxwelllhumbert <maxwell.humbert@stu.boone.kyschools.us>
2021-03-16 12:27:08 -0400

    LOBSTER TAKEOVER

--- Crab.java
+++ Crab.java
@@ -7,25 +7,48 @@ public class Crab extends Actor
 {
     public void act()
     {
-         if(isAtEdge() )
-         {
-             turn(50);
-         }
-         if(Greenfoot.getRandomNumber(100)<10)
-         {
-             turn(Greenfoot.getRandomNumber(910)-45);
-          }
+         
+         turnAtEdge();
+        randomTurn();
         move(20);
+        lookForWorm();
         
-        if (isTouching(Worm.class))
-        {
-            removeTouching(Worm.class);
-        }
+        
     }
     
     public void move()
     {
         move(5);
+    }
+    
+    /*
+         *check weather we stumbed upon a worm to see if he ate it if not leave it be
+         */
+    public void lookForWorm()
+    {
+        if(isTouching(Worm.class))
+        {
+            removeTouching(Worm.class);
+        }
+    }
+    
+   /*
+    * make te crab turn rigt between 0-45 degrees
+    */
+   public void randomTurn()
+   {
+       if(Greenfoot.getRandomNumber(100)<10)
+         {
+             turn(Greenfoot.getRandomNumber(910)-45);
+          }
+    }
+    
+    public void turnAtEdge()
+    {
+        if(isAtEdge() )
+         {
+             turn(50);
+         }
     }
 }
 
--- Lobster.java
+++ Lobster.java
@@ -8,12 +8,49 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Lobster extends Actor
 {
-    /**
-     * Act - do whatever the Lobster wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
+  public void act()
     {
-        // Add your action code here.
-    }    
+         
+         turnAtEdge();
+        randomTurn();
+        move(20);
+        lookForCrab();
+        
+        
+    }
+    
+    public void move()
+    {
+        move(5);
+    }
+    
+    /*
+         *check weather we stumbed upon a worm to see if he ate it if not leave it be
+         */
+    public void lookForCrab()
+    {
+        if(isTouching(Crab.class))
+        {
+            removeTouching(Crab.class);
+        }
+    }
+    
+   /*
+    * make te crab turn rigt between 0-45 degrees
+    */
+   public void randomTurn()
+   {
+       if(Greenfoot.getRandomNumber(100)<10)
+         {
+             turn(Greenfoot.getRandomNumber(910)-45);
+          }
+    }
+    
+    public void turnAtEdge()
+    {
+        if(isAtEdge() )
+         {
+             turn(50);
+         }
+    }
 }
